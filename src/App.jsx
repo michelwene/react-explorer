322c878296d94a8ea217bc69f1391483ed6ab270
Michel Wene <michelwene@hotmail.com>
2022-02-21 21:21:00 -0300

    styling list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { Counter } from "./components/Counter.jsx";
 import { RepositoryList } from "./components/RepositoryList.jsx";
 import "./styles/global.scss";
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
