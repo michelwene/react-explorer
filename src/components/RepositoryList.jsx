dfba6a82219fec54c94df9f02cd49918f0e09fd1
Michel Wene <michelwene@hotmail.com>
2022-02-22 21:38:22 -0300

    getting repositories api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem.jsx";
 
 import "../styles/repositories.scss";
+import { useState, useEffect } from "react";
 
 const repository = {
   name: "unform",
@@ -9,6 +10,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
